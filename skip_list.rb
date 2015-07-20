e7fd0e55e6bebbed7353e82c276ac708708a349d
Prajna <nihilism@me.com>
2015-07-20 17:43:14 +0800

    FEATURE - implement skip list

--- skip_list.rb
+++ skip_list.rb
@@ -1,3 +1,21 @@
 module SkipList
+  class Node
+    attr_accessor :forwards, :value
 
+    def initialize value, level
+      @value = value
+      forwards = Array.new level + 1
+    end
+  end
+
+  class SkipList
+    attr_accessor :header, :max_level
+
+    def initialize max_level
+      @max_level = max_level
+      @header = Node.new nil, max_level
+      sentinel = Node.new Float::INFINITY, max_level
+      @header.forwards = Array.new max_level + 1, sentinel 
+    end
+  end
 end
